c0238b4c1579b2651476e1f3fd144741b550a52d
Маргарита Смирнова <158091165+margarita0206@users.noreply.github.com>
2025-02-05 11:46:26 +0300

    accordion

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,10 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+import { acc } from './accordion';
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    acc();
+  });
+});
